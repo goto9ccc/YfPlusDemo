294aab69c4ae7673b24c7bdbf1c4b35b50564c27
goto9ccc <goto9ccc@gmail.com>
2015-12-18 13:28:19 +0800

    Signed-off-by: Song JinCheng

--- Android/app/src/main/java/com/winzxin/yfhelper/activity/LineChartActivity.java
+++ Android/app/src/main/java/com/winzxin/yfhelper/activity/LineChartActivity.java
@@ -1,5 +1,6 @@
 package com.winzxin.yfhelper.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ import java.util.List;
 
 import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
@@ -31,14 +33,6 @@ import lecho.lib.hellocharts.view.LineChartView;
 public class LineChartActivity extends AppCompatActivity {
     private LineChartView chart;
     private LineChartData data;
-    private int numberOfLines = 1;//线条数
-    private int maxNumberOfLines = 4;
-    private int numberOfPoints = 30;
-
-    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
-
-    private boolean hasAxes = true;
-    private boolean hasAxesNames = true;
     private boolean hasLines = true;
     private boolean hasPoints = true;
     private ValueShape shape = ValueShape.CIRCLE;
@@ -51,92 +45,64 @@ public class LineChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_chart);
-
-        chart = (LineChartView)findViewById(R.id.chart);
-        generateValues();
+        chart = (LineChartView) findViewById(R.id.chart);
         generateData();
-        resetViewport();
-        chart.setOnValueTouchListener(new ValueTouchListener());
-        chart.setViewportCalculationEnabled(false);
     }
 
-
-    private void generateValues() {
-        for (int i = 0; i < maxNumberOfLines; ++i) {
-            for (int j = 0; j < numberOfPoints; ++j) {
-                randomNumbersTab[i][j] = (float) Math.random() * 100f;
-            }
-        }
-    }
-
-    private void resetViewport() {
-
-//        final Viewport v = new Viewport(chart.getMaximumViewport());
-//        v.bottom = 0;
-//        v.top = 100;
-//        v.left = 0;
-//        v.right = numberOfPoints - 1;
-//        chart.setMaximumViewport(v);
-//        chart.setCurrentViewport(v);
-    }
 
     private void generateData() {
-
         final List<Line> lines = new ArrayList<>();
-        for (int i = 0; i < numberOfLines; ++i) {
-            final List<PointValue> values = new ArrayList<>();
-            Config config = Config.getInstance(this);
-            RequestQueue rq = Volley.newRequestQueue(this);
-            StringRequest sq = new StringRequest(config.getmServerUrl() + "/androidchart/saleline", new Response.Listener<String>() {
-                @Override
-                public void onResponse(String s) {
-                    try {
-                        JSONObject jsonObject = new JSONObject(s);
-                        JSONArray jsonArray = jsonObject.getJSONArray("data");
-                        for (int i = 0; i <jsonArray.length() ; i++) {
-                            JSONObject data = jsonArray.getJSONObject(i);
-                            values.add(new PointValue(i, ((float) data.getDouble("D1"))));
-                        }
-
-                        Line line = new Line(values);
-                        line.setColor(ChartUtils.COLORS[0]).setShape(shape).setCubic(isCubic)
-                                .setFilled(isFilled).setHasLabels(hasLabels)
-                                .setHasLabelsOnlyForSelected(hasLabelForSelected)
-                                .setHasLines(hasLines).setHasPoints(hasPoints);
-
-                        lines.add(line);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+        final List<PointValue> values = new ArrayList<>();
+        final List<AxisValue> axisValues = new ArrayList<>();
+        Config config = Config.getInstance(this);
+        RequestQueue rq = Volley.newRequestQueue(this);
+        StringRequest sq = new StringRequest(config.getmServerUrl()
+                + "/androidchart/saleline", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject data = jsonArray.getJSONObject(i);
+                        values.add(new PointValue(i, ((float) data.getDouble("D1"))));
+                        axisValues.add(new AxisValue((float) data.getDouble("D1"))
+                                .setLabel(data.getString("S1")));
                     }
 
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError volleyError) {
+                    Line line = new Line(values);
+                    line.setColor(ChartUtils.COLORS[0]).setShape(shape).setCubic(isCubic)
+                            .setFilled(isFilled).setHasLabels(hasLabels)
+                            .setHasLabelsOnlyForSelected(hasLabelForSelected)
+                            .setHasLines(hasLines).setHasPoints(hasPoints);
 
+                    lines.add(line);
+                    data = new LineChartData(lines);
+                    Axis axisX = new Axis();
+                    axisX.setHasTiltedLabels(true);
+                    axisX.setTextColor(Color.BLUE);
+                    axisX.setName("日期");
+                    //axisX.setMaxLabelChars(10);
+                    axisX.setValues(axisValues);
+                    data.setAxisXBottom(axisX);
+                    Axis axisY = new Axis().setHasLines(true);
+                    axisY.setName("金额");
+                    data.setAxisYLeft(axisY);
+                    data.setBaseValue(Float.NEGATIVE_INFINITY);
+                    chart.setLineChartData(data);
+                    chart.setOnValueTouchListener(new ValueTouchListener());
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            });
-            rq.add(sq);
 
-        }
-        data = new LineChartData(lines);
-        if (hasAxes) {
-            Axis axisX = new Axis();
-            Axis axisY = new Axis().setHasLines(true);
-            if (hasAxesNames) {
-                axisX.setName("Axis X");
-                axisY.setName("Axis Y");
             }
-            data.setAxisXBottom(axisX);
-            data.setAxisYLeft(axisY);
-        } else {
-            data.setAxisXBottom(null);
-            data.setAxisYLeft(null);
-        }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
 
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        chart.setLineChartData(data);
-
+            }
+        });
+        rq.add(sq);
     }
 
 
@@ -144,7 +110,8 @@ public class LineChartActivity extends AppCompatActivity {
 
         @Override
         public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
-            Toast.makeText(LineChartActivity.this, "选中: " + value, Toast.LENGTH_SHORT).show();
+            Toast.makeText(LineChartActivity.this, "选中: "
+                    + value, Toast.LENGTH_SHORT).show();
         }
 
         @Override
