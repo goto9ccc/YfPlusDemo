273c51acbc21093176d2b3752c5c325ff5cfabb0
goto9ccc <goto9ccc@gmail.com>
2015-12-22 12:29:01 +0800

    找了个模板套在前羰

--- DotNetMVC/YfWeb/App_Start/BundleConfig.cs
+++ DotNetMVC/YfWeb/App_Start/BundleConfig.cs
@@ -5,39 +5,36 @@ namespace YfWeb
 {
     public class BundleConfig
     {
-        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/xenon").Include(
+            "~/Scripts/bootstrap.min.js",
+            "~/Scripts/TweenMax.min.js",
+            "~/Scripts/resizeable.js",
+            "~/Scripts/joinable.j",
+            "~/Scripts/xenon-api.js",
+            "~/Scripts/xenon-toggles.js",
+            "~/Scripts/xenon-widgets.js",
+            "~/Scripts/globalize.min.js",
+            "~/Scripts/dx.chartjs.js",
+            "~/Scripts/toastr.min.js",
+            "~/Scripts/xenon-custom.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",
+                "~/Content/fonts/linecons/css/linecons.css",
+                "~/Content/fonts/fontawesome/css/font-awesome.min.css",
+                "~/Content/xenon-core.css",
+                "~/Content/xenon-forms.css",
+                "~/Content/xenon-components.css",
+                "~/Content/xenon-skins.css",
+                "~/Content/custom.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
