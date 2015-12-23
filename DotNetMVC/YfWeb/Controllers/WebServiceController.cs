4b289912f9672df3126108795d23c5cbbbf000ae
goto9ccc <goto9ccc@gmail.com>
2015-12-23 12:59:54 +0800

    song

---
+++ DotNetMVC/YfWeb/Controllers/WebServiceController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YfWeb.Common;
+using YfWeb.Models.DataModels;
+
+namespace YfWeb.Controllers
+{
+    public class WebServiceController : BaseController
+    {
+        //
+        // GET: /WebService/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult day_in_inv(string type = "day")
+        {
+            string sqlStr = "Select MOCTG.TG001 S1, MOCTG.TG011 D1,MOCTG.TG025 D2 FROM MOCTF "
+                                  + " left join MOCTG ON TF001 = TG001 AND TF002 = TG002 "
+                                  + " Where TF012 = '"
+                                  + DateTime.Now.ToString("yyyyMMdd")
+                                  + "' ";
+            switch (type)
+            {
+                default:
+                    break;
+                case "month": sqlStr = "Select MOCTG.TG001 S1, MOCTG.TG011 D1,MOCTG.TG025 D2 FROM MOCTF "
+                                   + " left join MOCTG ON TF001 = TG001 AND TF002 = TG002 "
+                                   + " Where TF012 >= '"
+                                   + DateTime.Now.ToString("yyyyMM")
+                                   + "01' and TF012 <=  '"
+                                  + DateTime.Now.ToString("yyyyMMdd")
+                                  + "' ";
+                    break;
+                case "year": sqlStr = "Select MOCTG.TG001 S1, MOCTG.TG011 D1,MOCTG.TG025 D2 FROM MOCTF "
+                       + " left join MOCTG ON TF001 = TG001 AND TF002 = TG002 "
+                       + " Where TF012 >= '"
+                       + DateTime.Now.ToString("yyyy")
+                       + "0101' and TF012 <=  '"
+                      + DateTime.Now.ToString("yyyyMMdd")
+                      + "' ";
+                    break;
+            }
+            try
+            {
+                var moctgList = db.Database.SqlQuery<PublicDataModuls>(sqlStr);
+                var moctgGroup = moctgList.GroupBy(u => u.S1).Select(g => new
+                {
+                    S1 = g.Key,
+                    D1 = g.Sum(x => x.D1),
+                    D2 = g.Sum(x => x.D2)
+                }).OrderByDescending(U => U.D1);
+                return Json(moctgGroup, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.ToString(), JsonRequestBehavior.AllowGet);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ DotNetMVC/YfWeb/Scripts/CustomerJS/Plus.js
@@ -0,0 +1,176 @@
+﻿
+function getInv(td001_02_03) {
+    $('#' + td001_02_03).show();
+    $('#' + td001_02_03).html('<img src="/Images/waiting.gif">');
+    $.getJSON('/yfs/sale/coptc_all_query?inv=' + td001_02_03,
+     function (data) {
+         $('#' + td001_02_03).html('');
+         $.each(data, function (key, obj) {
+             $('#' + td001_02_03).append('<P>仓库名称：' + obj.MC002 + '    数量：' + obj.MC007 + '    包装数量：' + obj.MC014 + '</P>');
+         });
+     });
+}
+
+
+function getMoc(td001_02_03) {
+
+    $('#' + td001_02_03).show();
+    $('#' + td001_02_03).html('<img src="/Images/waiting.gif">');
+    $.getJSON('/yfs/sale/coptc_all_query?moc=' + td001_02_03,
+     function (data) {
+         $('#' + td001_02_03).text('');
+         $.each(data, function (key, obj) {
+             str = '<table class="table">'
+             + '<tr>'
+             + '<tb>工单单别：</tb><tb>' + obj.TA001 + '-' + obj.TA002 + '</tb><tb>预计产量：</tb><tb>' + obj.TA015 + '/<tb><tb>已生产量：</tb><tb>' + obj.TA017 + '</tb>'
+             + '</tr>'
+             + '<tr>'
+             + '<tb>报废数量：</tb><tb>' + obj.TA018 + '</tb><tb>破坏数量：</tb><tb>' + obj.TA060 + '/<tb><th>预计开工日：</tb><tb>' + obj.TA019 + '</tb>'
+             + '</tr>'
+             + '<tr>'
+             + '<tb>预计完工日：</tb><tb>' + obj.TA010 + '</tb><tb>实际开工日：</tb><tb>' + obj.TA012 + '/<tb><tb>实际完工日：</tb><tb>' + obj.TA014 + '</tb>'
+             + '</tr>'
+             + '</table>';
+             $('#' + td001_02_03).append(str);
+         });
+     });
+}
+
+
+function divHide(td001_02_03) {
+
+    $('#' + td001_02_03).html('');
+}
+
+
+function headHide() {
+    if ($('#head').text() == '隐藏条件区域') {
+        $('#head').text('显示条件区域');
+        $('#orderList').hide();
+    }
+    else {
+        $('#head').text('隐藏条件区域');
+        $('#orderList').show();
+    };
+}
+
+function load(td001_02_03) {
+    $('#' + td001_02_03).show();
+    $('#' + td001_02_03).html('<img src="/Images/waiting.gif">');
+}
+
+function dy(td001_02_03, user, type) {
+    var $postUrl = "/yfs/dy/Add";
+
+    var $data = { "user": user, "billNo": td001_02_03, "type": type };
+    var $type = "json";
+    $.post($postUrl, $data, function (data) {
+        alert(data);
+    }, $type);
+}
+
+
+function dydel(td001_02_03, user, type) {
+    var $postUrl = "/yfs/dy/del";
+    var $data = { "user": user, "billNo": td001_02_03, "type": type };
+    var $type = "json";
+    $.post($postUrl, $data, function (data) {
+        alert(data);
+    }, $type);
+}
+
+
+function showLoading() {
+    $("body").append('<div id="info-loading"><div id="center-box"><div class="loading-alert-l"><img src="/Images/waiting.gif"> 数据处理中，请稍后</div></div></div>');
+    $("#info-loading").css('height', $(window).height());
+}
+function hideLoading() {
+    $("#info-loading").remove();
+}
+
+function OnSuccess(response) {
+    alert(response);
+}
+function OnFailure(response) {
+    alert("没找到对应的控制器，路径，方法");
+}
+
+function get_inv(id, url, first) {
+    if (first == true) {
+        $('#' + id).html('<img src="/Images/waiting.gif">');
+    };
+    $.getJSON('/yfs/INVService/day_in_inv?type=' + url,
+         function (data) {
+             $('#' + id).html('');
+             $.each(data, function (key, obj) {
+                 $('#' + id).append('<h5>单别：' + obj.TG001 + '    数量：' + obj.TG011 + '    包装数量：' + obj.TG025 + '</h5>');
+             });
+         });
+}
+
+function get_in_inv() {
+    get_inv('inv_day', 'day', true);
+    get_inv('inv_m', 'month', true);
+    get_inv('inv_y', 'year', true);
+    //sale("/yfs/Sale/COPTD", "cop_month");
+    //sale("/yfs/Sale/COPTD_lastMonth", "cop_last_month");
+    //sale("/yfs/Sale/", "sale");
+}
+
+function get_in_inv_time() {
+    get_inv('inv_day', 'day', false);
+    get_inv('inv_m', 'month', false);
+    get_inv('inv_y', 'year', false);
+    //sale("/yfs/Sale/COPTD", "cop_month");
+    //sale("/yfs/Sale/COPTD_lastMonth", "cop_last_month");
+    //sale("/yfs/Sale/", "sale");
+}
+
+function sale(URL, id) {
+    require.config({
+        paths: {
+            "echarts": "/Scripts/echarts/echarts",
+            "echarts/chart/map": "/Scripts/echarts/echarts-map"
+        }
+    });
+
+    require(
+        [
+            "echarts",
+            "echarts/chart/map"
+        ],
+        function (ec) {
+            var myChart = ec.init(document.getElementById(id));
+            $.getJSON(URL,
+                function (data) {
+                    myChart.setOption(data);
+                });
+
+        }
+    );
+}
+
+
+function load_zb(url, eid) {
+    $.get(url, function (data) {
+        $('#' + eid).html(data);
+    });
+}
+
+function order_zb(eid, url) {
+    $("#" + eid).html('<img src="/Images/waiting.gif">');
+    $.getJSON(url,
+        function (data) {
+            $("#" + eid).html('');
+            $("#" + eid).text(data + '天');
+        });
+}
+
+function loadbyid(eid) {
+    $("#" + eid).html('<img src="/Images/waiting.gif">');
+}
+
+function hideloadbyid(eid) {
+    $("#" + eid).html('');
+}
+
