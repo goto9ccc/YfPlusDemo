7202574a6ade20261254d3e8a55ede329f714197
goto9ccc <goto9ccc@gmail.com>
2016-01-17 00:34:29 +0800

    随便刷 刷 GIT

---
+++ DotNetMVC/YfWeb/Models/WebService/WebServiceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YfWeb.Models.Bean;
+
+namespace YfWeb.Models.WebService
+{
+    public class WebServiceModel:BaseModel
+    {
+
+
+    }
+}
